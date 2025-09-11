6c9a9ab7d6fca5b014472bf23ef471962b8d2ac1
MakayaCan <makayacanisio0@gmail.com>
2025-09-11 13:57:08 +0200

    Deleting records

--- website/views.py
+++ website/views.py
@@ -46,3 +46,23 @@ def register_user(request):
         form = SignUpForm()
 
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		#look up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page..")
+		return redirect('home')
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Records Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Delete!")
+		return redirect('home')
